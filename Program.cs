14e7030b8f7cac7f418a8d212f799444f7684e70
John Doe <john.doe@contoso.com>
2024-08-26 13:25:24 -0500

    first change

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container. making code changes
+
+//add new feature
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
